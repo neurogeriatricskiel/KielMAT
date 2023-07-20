ec83c37059d50123ab3a5d5829bce7727b0cbe32
Julius Welzel <julius.welzel@gmail.com>
2023-07-20 13:36:49 +0200

    [ADD] plotting of single acc data

--- import_data.py
+++ import_data.py
@@ -1,7 +1,9 @@
+import matplotlib.pyplot as plt
 from ngmt.motiondata import MotionData
 from pathlib import Path
 
-fpath = Path(r'C:\Users\User\Desktop\kiel\NGMT\examples\data\exDataHasomed.csv')
+fpath = Path(r'C:\Users\User\Desktop\kiel\NGMT\examples\data\exDataHasomed2.csv')
 hm_test = MotionData.import_hasomed_imu(fpath)
 
-hm_test.info
+# plot a channel
+plt.plot(hm_test.time_series[0:3,:].T)
