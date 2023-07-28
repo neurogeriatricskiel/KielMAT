610399485da54ed163e4094ebbcd7eb12a709bfa
JuliusWelzel <julius.welzel@gmail.com>
2023-07-28 20:42:05 +0200

    [ADD] exdata and import for polar data

--- import_data.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from ngmt.motiondata import MotionData
-from pathlib import Path
-
-fpath = Path(r'C:\Users\User\Desktop\kiel\NGMT\examples\data\exDataHasomed2.csv')
-hm_test = MotionData.import_hasomed_imu(fpath)
-
-# plot a channel
-plt.plot(hm_test.time_series[0:3,:].T)
