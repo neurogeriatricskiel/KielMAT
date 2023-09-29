b2e35b8c2c861611163c09796582e63db6267ec3
masoudabedinifar <140504378+masoudabedinifar@users.noreply.github.com>
2023-09-29 10:47:35 +0200

    Test file was updated.

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io
-import time
 from ngmt.datasets import keepcontrol, mobilised
 from ngmt.modules.gsd import GSDB
 from ngmt.modules.icd import ICDA
+import h5py
 
-start_time = time.time()
 def main():
     # User settings
-    FILE_NAME = 'C:\\Users\\Project\\Desktop\\Gait_Sequence\\Mobilise-D dataset_1-18-2023\\CHF\\data.mat'
+    FILE_NAME = 'C:\\Users\\Project\\Desktop\\Gait_Sequence\\Mobilise-D dataset_1-18-2023\\PFF\\data.mat'
 
     # Load IMU data from the MATLAB file
     ds = mobilised.load_file(file_name=FILE_NAME)
@@ -18,10 +17,13 @@ def main():
     imu_acc = [rec for rec in ds.devices[0].recordings if rec.type=='Acc'][0].data
     sampling_frequency = [rec for rec in ds.devices[0].recordings if rec.type=='Acc'][0].fs
 
+    # Use Gait_Sequence_Detection to detect gait sequence 
     gsd_output = GSDB.Gait_Sequence_Detection(imu_acceleration=imu_acc, sampling_frequency=sampling_frequency, plot_results=True)
+    
+    # Use Initial_Contact_Detection to detect intial contacts
     icd_output = ICDA.Initial_Contact_Detection(imu_acceleration=imu_acc, gait_sequences=gsd_output, sampling_frequency=sampling_frequency, plot_results=True)
+    
     return
-
 
 
 if __name__ == "__main__":
