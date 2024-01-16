88eff19de25bc0d507f6b661960247ee9b529600
Robbin Romijnders <r.j.romijnders@gmail.com>
2024-01-16 17:03:55 +0100

    Added quat2rotm and quat2axang to quaternion utils

--- ngmt/utils/quaternion.py
+++ ngmt/utils/quaternion.py
@@ -2,6 +2,9 @@ import numpy as np
 from typing import Optional
 
 
+_EPS = np.finfo("float").eps
+
+
 def quatinv(
     q: np.ndarray, scalar_first: bool = True, channels_last: bool = True
 ) -> np.ndarray:
@@ -281,12 +284,12 @@ def quatmultiply(
 
 
 def rotm2quat(
-    R: np.ndarray, scalar_first: bool = True, channels_last: bool = True
+    R: np.ndarray, method: int | str = "auto"
 ) -> np.ndarray:
     """Convert a 3x3 rotation matrix to a quaternion.
 
     Source:
-    - https://github.com/dlaidig/broad/blob/6738875895e821afda12a5dd1b186092c6cff6a4/example_code/broad_utils.py#L136
+    - https://github.com/dlaidig/qmt/blob/0fa8d32eb461e14d78e9ddbd569664ea59bcea19/qmt/functions/quaternion.py#L1004
 
     Parameters:
     - R (np.ndarray): A rotation matrix with shape (3, 3).
@@ -306,22 +309,145 @@ def rotm2quat(
     - The input arrays are cast to float before the computation.
     - If channels_last is False, the input arrays are transposed to switch channels and time axis.
     """
-    assert R.shape == (3, 3)
 
-    w_sq = (1 + R[0, 0] + R[1, 1] + R[2, 2]) / 4
-    x_sq = (1 + R[0, 0] - R[1, 1] - R[2, 2]) / 4
-    y_sq = (1 - R[0, 0] + R[1, 1] - R[2, 2]) / 4
-    z_sq = (1 - R[0, 0] - R[1, 1] + R[2, 2]) / 4
+    # Cast array to float
+    R = np.asarray(R, float)
+    assert R.ndim >= 2 and R.shape[-2:] == (3, 3)
 
-    q = np.zeros((4,), float)
-    if scalar_first:
-        q[0] = np.sqrt(w_sq)
-        q[1] = np.copysign(np.sqrt(x_sq), R[2, 1] - R[1, 2])
-        q[2] = np.copysign(np.sqrt(y_sq), R[0, 2] - R[2, 0])
-        q[3] = np.copysign(np.sqrt(z_sq), R[1, 0] - R[0, 1])
-    else:
-        q[0] = np.copysign(np.sqrt(x_sq), R[2, 1] - R[1, 2])
-        q[1] = np.copysign(np.sqrt(y_sq), R[0, 2] - R[2, 0])
-        q[2] = np.copysign(np.sqrt(z_sq), R[1, 0] - R[0, 1])
-        q[3] = np.sqrt(w_sq)
+    w_sq = (1 + R[..., 0, 0] + R[..., 1, 1] + R[..., 2, 2]) / 4
+    x_sq = (1 + R[..., 0, 0] - R[..., 1, 1] - R[..., 2, 2]) / 4
+    y_sq = (1 - R[..., 0, 0] + R[..., 1, 1] - R[..., 2, 2]) / 4
+    z_sq = (1 - R[..., 0, 0] - R[..., 1, 1] + R[..., 2, 2]) / 4
+
+    q = np.zeros(R.shape[:-2] + (4,), float)
+    if method == 'auto':  # use the largest value to avoid numerical problems
+        methods = np.argmax(np.array([w_sq, x_sq, y_sq, z_sq]), axis=0)
+    elif method == 'copysign':
+        q[..., 0] = np.sqrt(w_sq)
+        q[..., 1] = np.copysign(np.sqrt(x_sq), R[..., 2, 1] - R[..., 1, 2])
+        q[..., 2] = np.copysign(np.sqrt(y_sq), R[..., 0, 2] - R[..., 2, 0])
+        q[..., 3] = np.copysign(np.sqrt(z_sq), R[..., 1, 0] - R[..., 0, 1])
+    elif method not in (0, 1, 2, 3):
+        raise RuntimeError('invalid method, must be "copysign", "auto", 0, 1, 2 or 3')
+
+    if method == 0 or method == 'auto':
+        ind = methods == 0 if method == 'auto' else slice(None)
+        q[ind, 0] = np.sqrt(w_sq[ind])
+        q[ind, 1] = (R[ind, 2, 1] - R[ind, 1, 2]) / (4 * q[ind, 0])
+        q[ind, 2] = (R[ind, 0, 2] - R[ind, 2, 0]) / (4 * q[ind, 0])
+        q[ind, 3] = (R[ind, 1, 0] - R[ind, 0, 1]) / (4 * q[ind, 0])
+    if method == 1 or method == 'auto':
+        ind = methods == 1 if method == 'auto' else slice(None)
+        q[ind, 1] = np.sqrt(x_sq[ind])
+        q[ind, 0] = (R[ind, 2, 1] - R[ind, 1, 2]) / (4 * q[ind, 1])
+        q[ind, 2] = (R[ind, 1, 0] + R[ind, 0, 1]) / (4 * q[ind, 1])
+        q[ind, 3] = (R[ind, 0, 2] + R[ind, 2, 0]) / (4 * q[ind, 1])
+    if method == 2 or method == 'auto':
+        ind = methods == 2 if method == 'auto' else slice(None)
+        q[ind, 2] = np.sqrt(y_sq[ind])
+        q[ind, 0] = (R[ind, 0, 2] - R[ind, 2, 0]) / (4 * q[ind, 2])
+        q[ind, 1] = (R[ind, 1, 0] + R[ind, 0, 1]) / (4 * q[ind, 2])
+        q[ind, 3] = (R[ind, 2, 1] + R[ind, 1, 2]) / (4 * q[ind, 2])
+    if method == 3 or method == 'auto':
+        ind = methods == 3 if method == 'auto' else slice(None)
+        q[ind, 3] = np.sqrt(z_sq[ind])
+        q[ind, 0] = (R[ind, 1, 0] - R[ind, 0, 1]) / (4 * q[ind, 3])
+        q[ind, 1] = (R[ind, 0, 2] + R[ind, 2, 0]) / (4 * q[ind, 3])
+        q[ind, 2] = (R[ind, 2, 1] + R[ind, 1, 2]) / (4 * q[ind, 3])
     return q
+
+
+def quat2rotm(
+    q: np.ndarray,
+    scalar_first: bool = True,
+    channels_last: bool = True
+) -> np.ndarray:
+    """
+    Convert quaternion(s) to rotation matrix.
+
+    Parameters
+    ----------
+    q : np.ndarray
+        Input quaternion(s) as a NumPy array. The last dimension must have size 4.
+    scalar_first : bool, optional
+        If True, the quaternion is assumed to be in scalar-first order (default is True).
+    channels_last : bool, optional
+        If True, the last dimension represents the quaternion channels (default is True).
+        If False, the quaternion channels are assumed to be in the first dimension.
+
+    Returns
+    -------
+    np.ndarray
+        Rotation matrix corresponding to the input quaternion(s).
+
+    Raises
+    ------
+    AssertionError
+        If the last dimension of the input array `q` does not have size 4.
+
+    Notes
+    -----
+    The conversion is based on the formula:
+    R = | 1 - 2*q2^2 - 2*q3^2    2*(q1*q2 - q3*q0)    2*(q1*q3 + q2*q0) |
+        | 2*(q1*q2 + q3*q0)    1 - 2*q1^2 - 2*q3^2    2*(q2*q3 - q1*q0) |
+        | 2*(q1*q3 - q2*q0)    2*(q1*q0 + q2*q3)    1 - 2*q1^2 - 2*q2^2 |
+
+    References
+    ----------
+    - Wikipedia: https://en.wikipedia.org/wiki/Rotation_matrix#Quaternion
+
+    Examples
+    --------
+    >>> quaternion = np.array([1.0, 0.0, 0.0, 0.0])
+    >>> rotation_matrix = quat2rotm(quaternion)
+    """
+    
+    # Cast array to float
+    q = np.asarray(q, float)
+    assert q.shape[-1] == 4
+
+    # Derive rotation matrix from quaternion
+    R = np.zeros(q.shape[:-1] + (3, 3), float)
+    R[..., 0, 0] = 1 - 2 * q[..., 2] ** 2 - 2 * q[..., 3] ** 2
+    R[..., 0, 1] = 2 * (q[..., 1] * q[..., 2] - q[..., 3] * q[..., 0])
+    R[..., 0, 2] = 2 * (q[..., 1] * q[..., 3] + q[..., 2] * q[..., 0])
+    R[..., 1, 0] = 2 * (q[..., 1] * q[..., 2] + q[..., 3] * q[..., 0])
+    R[..., 1, 1] = 1 - 2 * q[..., 1] ** 2 - 2 * q[..., 3] ** 2
+    R[..., 1, 2] = 2 * (q[..., 2] * q[..., 3] - q[..., 1] * q[..., 0])
+    R[..., 2, 0] = 2 * (q[..., 1] * q[..., 3] - q[..., 2] * q[..., 0])
+    R[..., 2, 1] = 2 * (q[..., 1] * q[..., 0] + q[..., 2] * q[..., 3])
+    R[..., 2, 2] = 1 - 2 * q[..., 1] ** 2 - 2 * q[..., 2] ** 2
+    return R
+
+def quat2axang(q: np.ndarray) -> np.ndarray:
+    """
+    Convert a quaternion to axis-angle representation.
+
+    Parameters:
+    - q (np.ndarray): Input quaternion array of shape (..., 4).
+    
+    Returns:
+    - np.ndarray: Axis-angle representation array of shape (..., 4),
+                  where the first three elements are the axis of rotation
+                  and the last element is the angle of rotation in radians.
+
+    The function normalizes the input quaternion, calculates the angle of rotation,
+    and computes the axis of rotation in the axis-angle representation.
+
+    Note: The input quaternion array is expected to have the last dimension of size 4.
+    """
+
+    # Cast array to float
+    q = np.asarray(q, float)
+    assert q.shape[-1] == 4
+
+    # Normalize the quaternion
+    q = quatnormalize(q)
+
+    # Calculate the angle of rotation
+    axang = np.zeros_like(q)
+    axang[..., 3] = 2.0 * np.arccos(q[..., 0])
+    axang[..., :3] = np.where(np.sin(axang[..., 3] / 2.) > _EPS, 
+                              q[..., 1:] / np.sin(axang[..., 3] / 2.),
+                              np.array([0., 0., 1.]))
+    return axang
