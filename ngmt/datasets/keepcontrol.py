1af264f23f024519451129aa36e8adfd3b978f27
Robbin Romijnders <r.j.romijnders@gmail.com>
2024-01-18 11:29:01 +0100

    Updated Keep Control to load recording from dataset

--- ngmt/datasets/keepcontrol.py
+++ ngmt/datasets/keepcontrol.py
@@ -1,107 +1,69 @@
 import numpy as np
 import pandas as pd
+import pathlib
 import os
-import sys
-from ..utils.data_classes import (
-    FileInfo,
-    ChannelData,
-    EventData,
-    RecordingData,
-    MotionData,
-)
-
-_MAP_CHANNEL_TYPES = {"ACC": "ACCEL", "ANGVEL": "GYRO", "MAGN": "MAGN"}
-
-if sys.platform == "linux":
-    _BASE_PATH = "/mnt/neurogeriatrics_data/Keep Control/Data/lab dataset/rawdata"
-elif sys.platform == "win32":
-    _BASE_PATH = "Z:\\Keep Control\\Data\\lab dataset\\rawdata"
-else:
-    raise OSError(
-        "Currently only Windows- or Linux-based operating systems are supported!"
-    )
+from ngmt.utils.data_classes import NGMTRecording
 
 
-def load_file(file_path: str) -> MotionData:
+def load_recording(
+    file_name: str | pathlib.Path,
+    tracking_systems: str | list[str],
+    tracked_points: str | list[str] | dict[str, str] | dict[str, list[str]]
+):
+    """Load a recording from the Keep Control validation study.
+
+    Parameters
+    ----------
+    file_name : str | pathlib.Path
+        The absolute or relative path to the data file.
+    tracking_systems : str | list[str]
+        A string or list of strings of tracking systems for which data are to be returned.
+    tracked_points : str | list[str] | dict[str, str] | dict[str, list[str]]
+        Defines for which tracked points data are to be returned.
+        If a string or list of strings is provided, then these will be mapped to each requested tracking system.
+
+    Returns
+    -------
+    _ : NGMTRecording
+        An instance of the NGMTRecording dataclass.   
     """
-    Loads data from a given file from the Keep Control validation study.
+    # Put tracking systems in a list
+    if isinstance(tracking_systems, str):
+        tracking_systems = [tracking_systems]
+    
+    # Tracked points will be a dictionary mapping
+    # each tracking system to a list of tracked points of interest
+    if isinstance(tracked_points, str):
+        tracked_points = [tracked_points]
+    if isinstance(tracked_points, list):
+        tracked_points = {tracksys: tracked_points for tracksys in tracking_systems}
+    for k, v in tracked_points.items():
+        if isinstance(v, str):
+            tracked_points[k] = [v]
+        
+    # From the file_name, extract the tracking system
+    search_str = "_tracksys-"
+    idx_from = file_name.find(search_str) + len(search_str)
+    idx_to = idx_from + file_name[idx_from:].find("_")
+    current_tracksys = file_name[idx_from:idx_to]
 
-    The data is saved as a tab-separated text document, following the
-    BIDS motion guidelines.
+    # Initialize the data and channels dictionaroes
+    data_dict, channels_dict = {}, {}
+    for tracksys in tracking_systems:
 
-    Args:
-        file_path (str): Path to the data file (*.tsv).
+        # Set current filename
+        current_file_name = file_name.replace(f"{search_str}{current_tracksys}", f"{search_str}{tracksys}")
+        if os.path.isfile(current_file_name):
 
-    Returns:
-        MotionData: An instance of a `MotionData` object.
-    """
-    # Split path and file name
-    path_name, file_name = os.path.split(file_path)
+            # Read the data and channels info into a pandas DataFrame
+            df_data = pd.read_csv(current_file_name, header=0, sep="\t")
+            df_channels = pd.read_csv(current_file_name.replace("_motion.tsv", "_channels.tsv"), header=0, sep="\t")
 
-    # Get relevant infos
-    s = file_name.replace("sub-", "")
-    sub_id = s[: s.find("_task")]
-    s = s[s.find("_task") + len("_task") + 1 :]
-    if "_run" in file_name:
-        run_name = s[: s.find("_run")]
-        s = s[s.find("_run") + len("_run") + 1 :]
-    task_name = s[: s.find("_tracksys")]
-    s = s[s.find("_tracksys") + len("_tracksys") + 1 :]
-    tracksys = s[: s.find("_")]
+            # Now select only for the tracked points of interest
+            df_data = df_data.loc[:, [col for col in df_data.columns if any([tracked_point in col for tracked_point in tracked_points[tracksys]])]]
+            df_channels = df_channels[(df_channels["tracked_point"].isin(tracked_points[tracksys]))]
 
-    # Set the filename
-    _base_file_name = f"sub-{sub_id}_task-{task_name}_tracksys-{tracksys}"
-
-    # Set the file info
-    file_info = FileInfo(
-        SubjectID=sub_id,
-        TaskName=task_name,
-        DatasetName="Keep Control",
-        FilePath=os.path.join(_BASE_PATH, _base_file_name),
-    )
-
-    # Load the channels information
-    df_channels = pd.read_csv(
-        os.path.join(
-            _BASE_PATH, f"sub-{sub_id}", "motion", _base_file_name + "_channels.tsv"
-        ),
-        sep="\t",
-        header=0,
-    )
-    df_channels["type"] = df_channels["type"].map(_MAP_CHANNEL_TYPES)
-
-    # Set the channel data
-    channel_data = ChannelData(
-        name=df_channels["name"].to_list(),
-        component=df_channels["component"].to_list(),
-        ch_type=df_channels["type"].to_list(),
-        tracked_point=df_channels["tracked_point"].to_list(),
-        units=df_channels["units"].to_list(),
-        sampling_frequency=df_channels["sampling_frequency"].astype(float).to_list(),
-    )
-
-    # Load the data
-    df_data = pd.read_csv(
-        os.path.join(
-            _BASE_PATH, f"sub-{sub_id}", "motion", _base_file_name + "_motion.tsv"
-        ),
-        sep="\t",
-        header=0,
-    )
-
-    # Set the recording data
-    recording_data = RecordingData(
-        name=file_name,
-        data=df_data.values,
-        sampling_frequency=channel_data.sampling_frequency[0],
-        times=np.arange(len(df_data)) / channel_data.sampling_frequency[0],
-        channels=channel_data,
-        start_time=0.0,
-    )
-
-    return MotionData(
-        data=[recording_data],
-        times=recording_data.times,
-        info=[file_info],
-        ch_names=channel_data.name,
-    )
+            # Put data and channels in output dictionaries
+            data_dict[tracksys] = df_data
+            channels_dict[tracksys] = df_channels
+    return NGMTRecording(data=data_dict, channels=channels_dict)
