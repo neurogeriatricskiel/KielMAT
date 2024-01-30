85827e0a4f07ca1f8a76c3f884f27cc809883c2f
Julius Welzel <julius.welzel@gmail.com>
2024-01-30 14:54:21 +0100

    [ADD] json repair function in utils

--- ngmt/utils/file_io.py
+++ ngmt/utils/file_io.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+import json
+from json_repair import repair_json
+
 def get_unit_from_type(channel_types):
     """This function returns the unit of most common channel types automatically.
     For example ACCEL is always m/s^2, GYRO is always deg/s, MAG is always T, etc.
@@ -25,3 +29,26 @@ def get_unit_from_type(channel_types):
             units[i] = None
 
     return units
+
+def fix_json_file(file_path: str) -> None:
+    """
+    Fix a JSON file and save the fixed version.
+
+    Args:
+        file_path (str): The path to the JSON file.
+
+    Returns:
+        None
+    """
+    file_path = Path(file_path)
+    json_string = file_path.read_text()
+
+    try:
+        decoded_object = repair_json(json_string)
+        # save the decoded object as a JSON file with the original name
+        with open(file_path.name, "w") as write_file:
+            json_object = json.loads(decoded_object)
+            json.dump(json_object, write_file, indent=4)
+    except Exception:
+        # Not even this library could fix this JSON
+        print("Could not fix JSON file")
