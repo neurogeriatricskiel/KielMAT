65311fc5e192bb98d2f33916a95e7aa6b09688bd
masoudabedinifar <140504378+masoudabedinifar@users.noreply.github.com>
2024-03-15 10:07:53 +0100

    docstrings was updated for documentaion.

--- ngmt/modules/pam/_pam.py
+++ ngmt/modules/pam/_pam.py
@@ -7,34 +7,31 @@ from ngmt.utils import preprocessing
 
 class PhysicalActivityMonitoring:
     """
-    Monitors physical activity levels based on accelerometer data.
+    The algortihm monitors physical activity levels based on accelerometer data. It determines the 
+    intensity level of physical activities based on accelerometer signals using the following steps:
 
-    Description:
-        The algorithm determines the intensity level of physical activities based on accelerometer
-        signals using the following steps:
+    - Load Data: Includes a time index and accelerometer data (N, 3) for x, y, and z axes. The
+        sampling frequency (sampling_freq_Hz) is in Hz, with a default value of 100. Thresholds
+        (thresholds_mg) are provided as a dictionary containing threshold values for physical
+        activity detection in mg unit. The epoch duration (epoch_duration_sec) is defined in
+        seconds, with a default of 5 seconds. The last input is plot, which, if set to
+        True, generates a plot showing the average Euclidean Norm Minus One (ENMO) per hour for
+        each date. The default is True.
 
-        - Load Data: Includes a time index and accelerometer data (N, 3) for x, y, and z axes. The
-          sampling frequency (sampling_freq_Hz) is in Hz, with a default value of 100. Thresholds
-          (thresholds_mg) are provided as a dictionary containing threshold values for physical
-          activity detection in mg unit. The epoch duration (epoch_duration_sec) is defined in
-          seconds, with a default of 5 seconds. The last input is plot, which, if set to
-          True, generates a plot showing the average Euclidean Norm Minus One (ENMO) per hour for
-          each date. The default is True.
+    - Preprocess Signal: Calculate the sample-level Euclidean norm (EN) of the acceleration
+        signal. Apply a fourth-order Butterworth low-pass filter with a cut-off frequency of 20Hz
+        to remove noise. Calculate the Euclidean Norm Minus One (ENMO) index and truncate negative
+        values to zero. Convert the indices by multiplying them by 1000 to convert units from g to
+        mg.
 
-        - Preprocess Signal: Calculate the sample-level Euclidean norm (EN) of the acceleration
-          signal. Apply a fourth-order Butterworth low-pass filter with a cut-off frequency of 20Hz
-          to remove noise. Calculate the Euclidean Norm Minus One (ENMO) index and truncate negative
-          values to zero. Convert the indices by multiplying them by 1000 to convert units from g to
-          mg.
+    - Classify Intensity: Classify the intensity of physical activities based on the calculated
+        ENMO values using 5-second epochs. Thresholds for categorization are as follows: sedentary
+        activity < 45 mg, light activity 45–100 mg, moderate activity 100–400 mg, vigorous activity
+        > 400 mg.
 
-        - Classify Intensity: Classify the intensity of physical activities based on the calculated
-          ENMO values using 5-second epochs. Thresholds for categorization are as follows: sedentary
-          activity < 45 mg, light activity 45–100 mg, moderate activity 100–400 mg, vigorous activity
-          > 400 mg.
-
-        - Classify Activities: Classify different levels of activities and calculate the time spent
-          on each activity level for each day. If `plot` is True, the function generates a
-          plot showing the averaged ENMO values for each day.
+    - Classify Activities: Classify different levels of activities and calculate the time spent
+        on each activity level for each day. If `plot` is True, the function generates a
+        plot showing the averaged ENMO values for each day.
 
     Attributes:
         physical_activities_ (pd.DataFrame): DataFrame containing physical activity information for each day.
@@ -43,37 +40,38 @@ class PhysicalActivityMonitoring:
         detect(data, sampling_freq_Hz, thresholds_mg, epoch_duration_sec, plot):
             Detects gait sequences on the accelerometer signal.
 
-        __init__():
-            Initializes the physical activity instance.
+        Args:
+            data (pd.DataFrame): Input data with time index and accelerometer data (N, 3) for x, y, and z axes.
+            sampling_freq_Hz (float): Sampling frequency of the accelerometer data (in Hertz).
+            thresholds_mg (dict): Dictionary containing threshold values for physical activity detection.
+            epoch_duration_sec (int): Duration of each epoch in seconds.
+            plot (bool): If True, generates a plot showing the average Euclidean Norm Minus One (ENMO). Default is True.
 
-        Examples:
-            Determines physical activity levels in the sensor signal.
+        Returns:
+            PhysicalActivityMonitoring: An instance of the class with the physical activity levels information stored 
+            in the 'physical_activities_' attribute.
 
-            >>> pam = PhysicalActivityMonitoring()
-            >>> pam.detect(
-                    data=acceleration_data,
-                    sampling_freq_Hz=100,
-                    thresholds_mg={
-                        "sedentary_threshold": 45,
-                        "light_threshold": 100,
-                        "moderate_threshold": 400,
-                    },
-                    epoch_duration_sec=5,
-                    plot=True)
-            >>> physical_activities = pam.physical_activities_
-            >>> print(physical_activities)
-                           sedentary_mean_mg  sedentary_time_min  light_mean_mg  light_time_min  moderate_mean_mg  moderate_time_min  vigorous_mean_mg  vigorous_time_min
-            3/19/2018               23.48               733.08          60.78              72             146.2              21.58             730.34                0.58
-            3/20/2018               27.16               753.83          57.06           102.25            137.26               7.92             737.9                 0.42
+    Examples:
+        >>> pam = PhysicalActivityMonitoring()
+        >>> pam.detect(
+                data=acceleration_data,
+                sampling_freq_Hz=100,
+                thresholds_mg={
+                    "sedentary_threshold": 45,
+                    "light_threshold": 100,
+                    "moderate_threshold": 400,
+                },
+                epoch_duration_sec=5,
+                plot=True)
+        >>> print(pam.physical_activities_)
+                        sedentary_mean_mg  sedentary_time_min  light_mean_mg  light_time_min  moderate_mean_mg  moderate_time_min  vigorous_mean_mg  vigorous_time_min
+        3/19/2018               23.48               733.08          60.78              72             146.2              21.58             730.34                0.58
+        3/20/2018               27.16               753.83          57.06           102.25            137.26               7.92             737.9                 0.42
 
-        References:
-            [1] Doherty, Aiden, et al. (2017). Large scale population assessment of physical
-                activity using wrist-worn accelerometers: the UK biobank study. PloS one 12.2.
-                https://doi.org/10.1371/journal.pone.0169649
+    References:
+        [1] Doherty, Aiden, et al. (2017). Large scale population assessment of physical activity using wrist-worn accelerometers...
 
-            [2] Van Hees, Vincent T., et al. (2013). Separating movement and gravity components
-                in an acceleration signal and implications for the assessment of human daily physical
-                activity. PloS one 8.4. https://doi.org/10.1371/journal.pone.0061691
+        [2] Van Hees, Vincent T., et al. (2013). Separating movement and gravity components in an acceleration signal and implications...
     """
 
     def __init__(self):
