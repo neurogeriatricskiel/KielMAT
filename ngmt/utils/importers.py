92e2efd6108dca4c840202c0b4d0995d1913d22d
masoudabedinifar <140504378+masoudabedinifar@users.noreply.github.com>
2024-05-30 14:29:50 +0200

    handling different versions in APDM mobility lab importer.

--- ngmt/utils/importers.py
+++ ngmt/utils/importers.py
@@ -1,12 +1,13 @@
 import actipy
 import h5py
 import numpy as np
+import pandas as pd
 from ngmt.utils.ngmt_dataclass import NGMTRecording
 from ngmt.utils.file_io import get_unit_from_type
-import pandas as pd
 from pathlib import Path
+from typing import Union
 
-
+# Importher for Axivity
 def import_axivity(file_path: str, tracked_point: str):
     """
     Imports Axivity data from the specified file path and 
@@ -64,53 +65,50 @@ def import_axivity(file_path: str, tracked_point: str):
     return data, channels
 
 
-# Importher for APDM Mobility Lab system
-def import_mobilityLab(
-    file_name: str | Path,
-    tracked_points: str | list[str],
+# Importher for APDM Mobility Lab for different versions
+def import_apdm_mobilityLab(
+    file_name: str | Path, 
+    tracked_points: str | list[str]
 ) -> tuple[pd.DataFrame, pd.DataFrame]:
     """
-    Imports data from an APDM Mobility Lab system from the specified file path.
+    Imports data from an APDM Mobility Lab system's different versions from the specified file path.
 
     Args:
         file_name (str or Path): The absolute or relative path to the data file.
-        tracked_point (str or list of str]):
-            Defines for which tracked points data are to be returned.
+        tracked_points (str or list of str]): Defines for which tracked points data are to be returned.
 
     Returns:
-        dict, dict: The loaded data and channels as dictionaries.
+        tuple[pd.DataFrame, pd.DataFrame]: The loaded data and channels as dataframes.
 
     Examples:
         >>> file_path = "/path/to/sensor_data.h5"
         >>> tracked_point = "Lumbar"
-        >>> recording = import_mobilityLab(file_path, tracked_point)
+        >>> data, channels = import_mobilityLab_all(file_path, tracked_point)
     """
     # Convert file_name to a Path object if it is a string
     if isinstance(file_name, str):
         file_name = Path(file_name)
 
-    # Convert tracked_points into a list if the it is provided as a string
+    # Convert tracked_points into a list if it is provided as a string
     if isinstance(tracked_points, str):
         tracked_points = [tracked_points]
 
-    with h5py.File(file_name, 'r') as hfile:
-        # Get monitor labels and case IDs
-        monitor_labels = hfile.attrs['MonitorLabelList']
-        monitor_labels = [
-            s.decode("UTF-8").strip()
-            for s in monitor_labels
-        ]
-        case_ids = hfile.attrs['CaseIdList']
-        case_ids = [
-            s.decode("UTF-8")[:9]
-            for s in case_ids
-        ]
-        
-        # Track invalid tracked points
-        invalid_tracked_points = [tp for tp in tracked_points if tp not in monitor_labels]
+    # Mapping of sensor types to make them consistent with NGMT dataclass definition
+    sensor_type_mapping = {
+        'Accelerometer': 'ACCEL',
+        'Gyroscope': 'GYRO',
+        'Magnetometer': 'MAGN',
+        'Accelerometers': 'ACCEL',
+        'Gyroscopes': 'GYRO',
+        'Magnetometers': 'MAGN'
+    }
 
-        if invalid_tracked_points:
-            raise ValueError(f"The following tracked points do not exist in monitor labels: {invalid_tracked_points}")
+    with h5py.File(file_name, 'r') as hfile:
+        # Check if there is an attribute or dataset that indicates the version
+        if 'FileFormatVersion' in hfile.attrs:
+            version = hfile.attrs['FileFormatVersion']
+        else:
+            raise ValueError("Version attribute not found in the h5 file.")
 
         # Initialize dictionaries to store channels and data frames
         channels_dict = {
@@ -121,55 +119,99 @@ def import_mobilityLab(
             "units": [],
             "sampling_frequency": []
         }
-
-        # Create dictionary to store data
         data_dict = {}
 
-        # Iterate over each sensor
-        for idx_sensor, (monitor_label, case_id) in enumerate(zip(monitor_labels, case_ids)):
-            if monitor_label not in tracked_points:
-                continue  # to next sensor name
-            sample_rate = hfile[case_id].attrs['SampleRate']
-            
-            # Get raw data
-            rawAcc = hfile[case_id]['Calibrated']['Accelerometers'][:]
-            rawGyro = hfile[case_id]['Calibrated']['Gyroscopes'][:]
-            rawMagn = hfile[case_id]['Calibrated']['Magnetometers'][:]
+        # Check the version
+        if version == 5:
+            sensors_group = hfile['Sensors']
+            sensor_names = list(sensors_group.keys())
 
-            # Populate data_dict
-            data_dict[f'{monitor_label}'] = pd.DataFrame({
-                f'{monitor_label}_ACCEL_x': rawAcc[:,0],
-                f'{monitor_label}_ACCEL_y': rawAcc[:,1],
-                f'{monitor_label}_ACCEL_z': rawAcc[:,2],
-                f'{monitor_label}_GYRO_x': rawGyro[:,0],
-                f'{monitor_label}_GYRO_y': rawGyro[:,1],
-                f'{monitor_label}_GYRO_z': rawGyro[:,2],
-                f'{monitor_label}_MAGN_x': rawMagn[:,0],
-                f'{monitor_label}_MAGN_y': rawMagn[:,1],
-                f'{monitor_label}_MAGN_z': rawMagn[:,2],
-            })
+            # Mapping of sensor names to monitor labels for version 5
+            sensor_to_label = {
+                "15403": "Right Leg",
+                "15514": "Left Leg",
+                "15520": "Left Arm",
+                "16159": "Lumbar",
+                "16304": "Right Arm",
+                "16608": "Trunk"
+            }
 
-            # Extend lists in channels_dict
-            channels_dict["name"].extend([
-                f"{monitor_label}_ACCEL_x",
-                f"{monitor_label}_ACCEL_y",
-                f"{monitor_label}_ACCEL_z",
-                f"{monitor_label}_GYRO_x",
-                f"{monitor_label}_GYRO_y",
-                f"{monitor_label}_GYRO_z",
-                f"{monitor_label}_MAGN_x",
-                f"{monitor_label}_MAGN_y",
-                f"{monitor_label}_MAGN_z",
-            ])
+            # Convert tracked_points to sensor IDs using sensor_to_label mapping
+            tracked_points = [sensor for sensor, label in sensor_to_label.items() if label in tracked_points]
 
-            channels_dict["component"].extend(['x', 'y', 'z'] * 3)
-            channels_dict["type"].extend(['ACCEL', 'ACCEL', 'ACCEL', 'GYRO', 'GYRO', 'GYRO', 'MAGN', 'MAGN', 'MAGN'])
-            channels_dict["tracked_point"].extend([monitor_label] * 9)
-            channels_dict["units"].extend(['m/s^2', 'm/s^2', 'm/s^2', 'rad/s', 'rad/s', 'rad/s', 'µT', 'µT', 'µT'])
-            channels_dict["sampling_frequency"].extend([sample_rate] * 9)
+            # Track invalid tracked points
+            invalid_tracked_points = [tp for tp in tracked_points if tp not in sensor_names]
 
-    # Concatenate data frames from data_dict
-    data = pd.concat(list(data_dict.values()), axis=1)
+            if invalid_tracked_points:
+                raise ValueError(f"The following tracked points do not exist in sensor names: {invalid_tracked_points}")
+
+            # Iterate over each sensor
+            for sensor_name in sensor_names:
+                if sensor_name not in tracked_points:
+                    continue  # to next sensor name
+
+                sensor_data = sensors_group[sensor_name]
+                sample_rate = sensor_data['Configuration'].attrs['Sample Rate']
+                label = sensor_to_label[sensor_name]
+
+                # Extract and append sensor data to the DataFrame
+                for axis_label in ['x', 'y', 'z']:
+                    for sensor_type in ['Accelerometer', 'Gyroscope', 'Magnetometer']:
+                        column_name = f"{label}_{sensor_type_mapping[sensor_type]}_{axis_label}"
+                        if sensor_type in sensor_data:
+                            raw_data = sensor_data[sensor_type][:]
+                            data_dict[column_name] = raw_data[:, 'xyz'.index(axis_label)]
+
+                            # Extend lists in channels_dict
+                            channels_dict["name"].append(column_name)
+                            channels_dict["component"].append(axis_label)
+                            channels_dict["type"].append(sensor_type_mapping[sensor_type])
+                            channels_dict["tracked_point"].append(label)
+                            channels_dict["units"].append(sensor_data[sensor_type].attrs['Units'].decode())
+                            channels_dict["sampling_frequency"].append(sample_rate)
+
+        else:
+            # Structure for version 3 and 4
+            monitor_labels = hfile.attrs['MonitorLabelList']
+            monitor_labels = [s.decode("UTF-8").strip() for s in monitor_labels]
+            case_ids = hfile.attrs['CaseIdList']
+            case_ids = [s.decode("UTF-8")[:9] for s in case_ids]
+
+            # Track invalid tracked points
+            invalid_tracked_points = [tp for tp in tracked_points if tp not in monitor_labels]
+
+            if invalid_tracked_points:
+                raise ValueError(f"The following tracked points do not exist in monitor labels: {invalid_tracked_points}")
+
+            # Iterate over each sensor
+            for idx_sensor, (monitor_label, case_id) in enumerate(zip(monitor_labels, case_ids)):
+                if monitor_label not in tracked_points:
+                    continue  # Skip to next sensor name
+
+                sample_rate = hfile[case_id].attrs['SampleRate']
+                sensor_data = hfile[case_id]['Calibrated']
+                
+                # Extract data for Accelerometers, Gyroscopes, and Magnetometers
+                sensor_types = ['Accelerometers', 'Gyroscopes', 'Magnetometers']
+                for sensor_type in sensor_types:
+                    if sensor_type in sensor_data:
+                        raw_data = sensor_data[sensor_type][:]
+                        units = sensor_data[sensor_type].attrs['Units'].decode()
+                        
+                        for axis_label in ['x', 'y', 'z']:
+                            column_name = f"{monitor_label}_{sensor_type_mapping[sensor_type]}_{axis_label}"
+                            data_dict[column_name] = raw_data[:, 'xyz'.index(axis_label)]
+
+                            # Extend lists in channels_dict
+                            channels_dict["name"].append(column_name)
+                            channels_dict["component"].append(axis_label)
+                            channels_dict["type"].append(sensor_type_mapping[sensor_type])
+                            channels_dict["tracked_point"].append(monitor_label)
+                            channels_dict["units"].append(units)
+                            channels_dict["sampling_frequency"].append(sample_rate)
+
+    # Create DataFrame from data_dict
+    data = pd.DataFrame(data_dict)
     
     # Create DataFrame from channels_dict
     channels = pd.DataFrame(channels_dict)
